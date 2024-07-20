6a737cf3923d0f1e30fa4ad2dd68a682d5488a81
MOHAMEDIT-DEV <mohamed.developpeur@gmail.com>
2024-07-20 20:57:54 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, Xavki!");
         System.out.println("Hello, Hamada!");
+        System.out.println("Hello, everyone!");
     }
 }
 
